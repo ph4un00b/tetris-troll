d27d839fd4e6549d40933c4b06bb5dec3e15f4fb
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-08-19 08:34:23 -0600

    collision + game over

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,31 @@ struct Coso {
     x: f32,
     y: f32,
 }
+impl Coso {
+    fn collides_with(&self, other: &Self) -> bool {
+        self.rect().overlaps(&other.rect())
+    }
+
+    //? el cuadro que mapea la colisión❗
+    /*
+     * Rect also starts from the upper left corner, so we must too here subtract half
+     * the stork from both X and Y.
+     *
+     * phau: falta un debug mode para ver el perímetro❗
+     */
+    fn rect(&self) -> Rect {
+        Rect {
+            x: self.x - self.size / 2.0,
+            y: self.y - self.size / 2.0,
+            w: self.size,
+            h: self.size,
+        }
+    }
+}
 
 #[macroquad::main("TetrisTroll")]
 async fn main() {
+    let mut game_over = false;
     const MOVEMENT_SPEED: f32 = 200.0;
 
     rand::srand(miniquad::date::now() as u64);
@@ -26,48 +48,73 @@ async fn main() {
     //?  Macroquad will clear the screen at the beginning of each frame.
     loop {
         clear_background(DARKPURPLE);
+        if !game_over {
+            //? input handlers❗
+            // * @see https://docs.rs/macroquad/latest/macroquad/input/enum.KeyCode.html
+            let delta_time = get_frame_time();
+            if is_key_down(KeyCode::Right) {
+                circle.x += MOVEMENT_SPEED * delta_time;
+            }
+            if is_key_down(KeyCode::Left) {
+                circle.x -= MOVEMENT_SPEED * delta_time;
+            }
+            if is_key_down(KeyCode::Down) {
+                circle.y += MOVEMENT_SPEED * delta_time;
+            }
+            if is_key_down(KeyCode::Up) {
+                circle.y -= MOVEMENT_SPEED * delta_time;
+            }
 
-        //? input handlers❗
-        // * @see https://docs.rs/macroquad/latest/macroquad/input/enum.KeyCode.html
-        let delta_time = get_frame_time();
-        if is_key_down(KeyCode::Right) {
-            circle.x += MOVEMENT_SPEED * delta_time;
-        }
-        if is_key_down(KeyCode::Left) {
-            circle.x -= MOVEMENT_SPEED * delta_time;
-        }
-        if is_key_down(KeyCode::Down) {
-            circle.y += MOVEMENT_SPEED * delta_time;
-        }
-        if is_key_down(KeyCode::Up) {
-            circle.y -= MOVEMENT_SPEED * delta_time;
+            //? Clamp X and Y to be within the screen
+            circle.x = circle.x.min(screen_width()).max(0.0);
+            circle.y = circle.y.min(screen_height()).max(0.0);
+
+            draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+            draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+            draw_circle(x - 30.0, y - 30.0, 45.0, BROWN);
+
+            draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+            //? game world❗
+
+            //? instances
+            if rand::gen_range(0, 99) >= 95 {
+                let size = rand::gen_range(16.0, 64.0);
+                squares.push(Coso {
+                    size,
+                    speed: rand::gen_range(50.0, 150.0),
+                    x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                    y: -size,
+                });
+            }
+            //? move instances
+            for cosito in &mut squares {
+                cosito.y += cosito.speed * delta_time;
+            }
+
+            //? optimization: Remove squares below bottom of screen
+            squares.retain(|square| square.y < screen_width() + square.size);
         }
 
-        //? Clamp X and Y to be within the screen
-        circle.x = circle.x.min(screen_width()).max(0.0);
-        circle.y = circle.y.min(screen_height()).max(0.0);
-
-        //? instances
-        if rand::gen_range(0, 99) >= 95 {
-            let size = rand::gen_range(16.0, 64.0);
-            squares.push(Coso {
-                size,
-                speed: rand::gen_range(50.0, 150.0),
-                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
-                y: -size,
-            });
+        //? check collisions
+        if squares.iter().any(|square| circle.collides_with(square)) {
+            game_over = true;
         }
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(x - 30.0, y - 30.0, 45.0, BROWN);
-
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-        //? game world❗
-        for cosito in &mut squares {
-            cosito.y += cosito.speed * delta_time;
+        //? restart
+        if game_over && is_key_pressed(KeyCode::Space) {
+            /*
+             * The difference between is_key_down() and is_key_pressed()
+             * is that the latter only checks if the key was pressed below
+             * the current frame while it previously apply to all frames that
+             * the button is pressed.
+             *
+             * There is also is_key_released() which
+             * checks if the key was released during the current one frame.
+             */
+            squares.clear();
+            circle.x = screen_width() / 2.0;
+            circle.y = screen_height() / 2.0;
+            game_over = false;
         }
-        //? optimization: Remove squares below bottom of screen
-        squares.retain(|square| square.y < screen_width() + square.size);
 
         draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
         for cosito in &squares {
@@ -77,6 +124,18 @@ async fn main() {
                 cosito.size,
                 cosito.size,
                 PINK,
+            );
+        }
+        //? game over text❗
+        if game_over {
+            let text = "Game Over!";
+            let text_dimensions = measure_text(text, None, 50, 1.0);
+            draw_text(
+                text,
+                screen_width() / 2.0 - text_dimensions.width / 2.0,
+                screen_height() / 2.0,
+                80.0,
+                RED,
             );
         }
         next_frame().await
