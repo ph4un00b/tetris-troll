47169d4d0d043fe199989287258d534a90f22742
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-08-19 17:21:54 -0600

    explosion + particles

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::{miniquad::date::now, prelude::*};
+use macroquad_particles::{self as part, ColorCurve, Emitter, EmitterConfig};
 //todo: fix shader for mobile❗
 const FRAGMENT_SHADER: &str = include_str!("starfield-shader.glsl");
 
@@ -29,6 +30,7 @@ struct Coso {
     speed: f32,
     x: f32,
     y: f32,
+    collided: bool,
 }
 impl Coso {
     fn collides_with(&self, other: &Self) -> bool {
@@ -69,6 +71,7 @@ enum Evt {
 #[macroquad::main("TetrisTroll")]
 async fn main() {
     simulate_mouse_with_touch(true);
+    //? game shader init
     let direction_modifier: f32 = 0.0;
     let render_target = render_target(320, 150);
     render_target.texture.set_filter(FilterMode::Nearest);
@@ -87,9 +90,11 @@ async fn main() {
         },
     )
     .unwrap();
-
+    //? game inits
+    let mut explosions: Vec<(Emitter, Vec2)> = vec![];
     let mut game_state = GS::Main;
     let mut game_taps = Evt::None;
+    let mut game_over_at = 0.0;
     const MOVEMENT_SPEED: f32 = 200.0;
 
     rand::srand(now() as u64);
@@ -99,6 +104,7 @@ async fn main() {
         speed: MOVEMENT_SPEED,
         x: screen_width() / 2.0,
         y: screen_height() / 2.0,
+        collided: false,
     };
 
     let x = screen_width() / 2.0;
@@ -194,11 +200,11 @@ async fn main() {
             (GS::Main | GS::Paused | GS::GameOver, Evt::None | Evt::Tapped(_, _)) => {
                 for touch in touches() {
                     let (fill_color, size) = match touch.phase {
-                        TouchPhase::Started => (GREEN, 90.0),
-                        TouchPhase::Stationary => (WHITE, 90.0),
-                        TouchPhase::Moved => (YELLOW, 90.0),
-                        TouchPhase::Ended => (BLUE, 90.0),
-                        TouchPhase::Cancelled => (BLACK, 90.0),
+                        TouchPhase::Started => (GREEN, 20.0),
+                        TouchPhase::Stationary => (WHITE, 20.0),
+                        TouchPhase::Moved => (YELLOW, 20.0),
+                        TouchPhase::Ended => (BLUE, 20.0),
+                        TouchPhase::Cancelled => (BLACK, 20.0),
                     };
                     draw_circle(touch.position.x, touch.position.y, size, fill_color);
                 }
@@ -240,6 +246,8 @@ async fn main() {
                  * checks if the key was released during the current one frame.
                  */
                 squares.clear();
+                explosions.clear();
+
                 player.x = screen_width() / 2.0;
                 player.y = screen_height() / 2.0;
 
@@ -278,6 +286,7 @@ async fn main() {
                         speed: rand::gen_range(50.0, 150.0),
                         x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
                         y: -size,
+                        collided: false,
                     });
                 }
                 //? move instances
@@ -285,17 +294,39 @@ async fn main() {
                     cosito.y += cosito.speed * delta_time;
                 }
                 //? optimization: Remove squares below bottom of screen
+                squares.retain(|square| !square.collided);
                 squares.retain(|square| square.y < screen_width() + square.size);
+                // todo hay un problema con eliminar muy rápido la explosiones
+                // explosions.retain(|(explosion, _)| explosion.config.emitting);
                 //? world
                 draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
                 draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
                 draw_circle(x - 30.0, y - 30.0, 45.0, BROWN);
                 draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
                 //? check collisions
-                if squares.iter().any(|square| player.collides_with(square)) {
+                for square in squares.iter_mut() {
+                    if !player.collided && player.collides_with(square) {
+                        player.collided = true;
+                        square.collided = true;
+                        game_over_at = now() + 1.25;
+                        explosions.push((
+                            Emitter::new(EmitterConfig {
+                                amount: square.size.round() as u32 * 4,
+                                ..particle_explosion()
+                            }),
+                            vec2(square.x, square.y),
+                        ));
+                    }
+                }
+
+                // if squares.iter().any(|square| player.collides_with(square)) {
+                if player.collided && now() > game_over_at {
                     game_state = GS::GameOver;
                 }
                 //? drawing
+                for (explosion, coords) in explosions.iter_mut() {
+                    explosion.draw(*coords);
+                }
                 for touch in touches() {
                     (player.x, player.y) = (touch.position.x, touch.position.y);
 
@@ -363,5 +394,37 @@ async fn main() {
         }
 
         next_frame().await
+    }
+}
+
+fn particle_explosion() -> part::EmitterConfig {
+    /*
+     * We will use the same configuration for all explosions,
+     * and only will resize it based on the size of the square.
+     *
+     * Therefore, we create a function returning one EmitterConfig which
+     * can be used to create one Emitter. One Emitter is a point based on
+     * particles can be generated.
+     *
+     * @see https://docs.rs/macroquad-particles/latest/macroquad_particles/struct.EmitterConfig.html
+     */
+    part::EmitterConfig {
+        local_coords: false,
+        one_shot: true,
+        emitting: true,
+        lifetime: 0.6,
+        lifetime_randomness: 0.3,
+        explosiveness: 0.65,
+        initial_direction_spread: 2.0 * std::f32::consts::PI,
+        initial_velocity: 300.0,
+        initial_velocity_randomness: 0.8,
+        size: 3.0,
+        size_randomness: 0.3,
+        colors_curve: ColorCurve {
+            start: VIOLET,
+            mid: ORANGE,
+            end: GREEN,
+        },
+        ..Default::default()
     }
 }
