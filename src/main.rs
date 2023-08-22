5e49d4e18caff134a75f987785d3a8f2a788d4a3
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-08-21 21:27:53 -0600

    refactor: naming

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
-use constants::MOVEMENT_SPEED;
-use enemies::Enemies;
-
-use macroquad::audio::{load_sound, play_sound, play_sound_once, stop_sound, PlaySoundParams};
-
-use macroquad::{miniquad::date::now, prelude::*};
-
-use shared::Organism;
-use pointers::Pointers;
-use ui::UI;
-use universe::Universe;
-
 use crate::player::Player;
 use crate::shared::Coso;
+use constants::MOVEMENT_SPEED;
+use enemies::Enemies;
+use macroquad::audio::{load_sound, play_sound, play_sound_once, stop_sound, PlaySoundParams};
+use macroquad::{miniquad::date::now, prelude::*};
+use pointers::Pointers;
+use shared::Organism;
+use ui::UI;
+use universe::Universe;
 mod constants;
 mod enemies;
 mod enemy;
 mod player;
-mod shared;
 mod pointers;
+mod shared;
 mod ui;
 mod universe;
 //todo: fix shader for mobile❗
 const FRAGMENT_SHADER: &str = include_str!("starfield-shader.glsl");
-
 /*
  * Macroquad automatically adds some uniforms to shaders.
  * The ones that exist available
@@ -49,14 +44,14 @@ enum GS {
     Main,
     Playing,
     Paused,
-    GameOver,
+    Dead,
 }
 
 #[derive(PartialEq)]
 enum Evt {
     None,
-    Tapped(f64, f64),
-    DoubleTapped,
+    Tap(f64, f64),
+    DTap,
 }
 #[macroquad::main("TetrisTroll")]
 async fn main() {
@@ -128,21 +123,21 @@ async fn main() {
         for touch in touches() {
             if let TouchPhase::Started = touch.phase {
                 game_taps = match game_taps {
-                    Evt::None => Evt::Tapped(now(), 0.250),
-                    Evt::Tapped(init, delay) if now() > (init + delay) => Evt::Tapped(now(), delay),
-                    Evt::Tapped(_, _) => Evt::DoubleTapped,
-                    Evt::DoubleTapped => Evt::Tapped(now(), 0.250),
+                    Evt::None => Evt::Tap(now(), 0.250),
+                    Evt::Tap(init, delay) if now() > (init + delay) => Evt::Tap(now(), delay),
+                    Evt::Tap(_, _) => Evt::DTap,
+                    Evt::DTap => Evt::Tap(now(), 0.250),
                 };
             };
         }
 
         match &game_taps {
             Evt::None => UI::debug_touch(),
-            Evt::Tapped(init, _delay) => UI::debug_tap(init),
-            Evt::DoubleTapped => UI::debug_double_tap(),
+            Evt::Tap(init, _delay) => UI::debug_tap(init),
+            Evt::DTap => UI::debug_double_tap(),
         };
 
-        if let (GS::Main | GS::Paused | GS::GameOver, Evt::None | Evt::Tapped(_, _)) =
+        if let (GS::Main | GS::Paused | GS::Dead, Evt::None | Evt::Tap(_, _)) =
             (&game_state, &game_taps)
         {
             Pointers::draw();
@@ -152,14 +147,14 @@ async fn main() {
             (GS::Main, Evt::None) => {
                 UI::touch_window();
             }
-            (GS::Main, Evt::Tapped(_, _)) => UI::main_window(|| {
+            (GS::Main, Evt::Tap(_, _)) => UI::main_window(|| {
                 enemies.reset();
                 player.reset();
                 game_state = GS::Playing;
                 game_taps = Evt::None;
             }),
-            (GS::Main, Evt::DoubleTapped) => {}
-            (GS::Playing, Evt::None | Evt::Tapped(_, _)) => {
+            (GS::Main, Evt::DTap) => {}
+            (GS::Playing, Evt::None | Evt::Tap(_, _)) => {
                 if !main_music_started {
                     main_music_started = true;
                     //todo: It may be a little intense that the music starts at
@@ -182,39 +177,39 @@ async fn main() {
                     game_over_at = now() + 1.25;
                 }
                 if player.props.collided && now() > game_over_at {
-                    game_state = GS::GameOver;
+                    game_state = GS::Dead;
                 }
                 enemies.draw();
                 player.draw();
                 Universe::draw();
             }
-            (GS::Playing, Evt::DoubleTapped) => {
+            (GS::Playing, Evt::DTap) => {
                 play_sound_once(&transition_sound);
                 stop_sound(&theme_music);
                 game_state = GS::Paused;
                 game_taps = Evt::None;
             }
-            (GS::Paused, Evt::None | Evt::Tapped(_, _)) => {
+            (GS::Paused, Evt::None | Evt::Tap(_, _)) => {
                 if is_key_pressed(KeyCode::Escape) {
                     game_state = GS::Playing;
                 }
                 UI::game_paused();
             }
-            (GS::Paused, Evt::DoubleTapped) => {
+            (GS::Paused, Evt::DTap) => {
                 play_sound_once(&transition_sound);
                 main_music_started = false;
                 game_state = GS::Playing;
                 game_taps = Evt::None;
             }
-            (GS::GameOver, Evt::None | Evt::Tapped(_, _)) => {
+            (GS::Dead, Evt::None | Evt::Tap(_, _)) => {
                 stop_sound(&theme_music);
                 main_music_started = false;
                 UI::game_over_window(|| {
                     game_state = GS::Main;
-                    game_taps = Evt::Tapped(now(), 0.250);
+                    game_taps = Evt::Tap(now(), 0.250);
                 })
             }
-            (GS::GameOver, Evt::DoubleTapped) => {}
+            (GS::Dead, Evt::DTap) => {}
         }
 
         next_frame().await
