bc38702a0109e5e1b6c0984d9fac449f2deb57b1
phaunus <1057021+ph4un00b@users.noreply.github.com>
2023-08-28 13:34:01 -0600

    movement

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,11 @@
 use macroquad::prelude::{vec2, Color, Vec2, BLACK};
 
-pub const MOVEMENT_SPEED: f32 = 200.0;
+pub const MOVEMENT_SPEED: f32 = 5.0;
 pub const WINDOWS_SIZE: Vec2 = vec2(1470.0, 420.0);
 pub const DEBUG_COLOR: Color = BLACK;
+pub const ROWS: usize = 10;
+pub const COLUMNS: usize = 24;
+
 // pub const ASPECT_RATIO: f32 = WINDOWS_SIZE.x / WINDOWS_SIZE.y;
 // pub const BLOCK_SIZE: Vec2 = vec2(
 //     WINDOWS_SIZE.x * ASPECT_RATIO / 10.0,
---
+++ src/tetromino.rs
@@ -0,0 +1,96 @@
+use macroquad::{
+    prelude::{Rect, Vec2, DARKBLUE, DARKGREEN, ORANGE, PURPLE, RED, SKYBLUE, YELLOW},
+    shapes::draw_circle,
+    time::get_frame_time,
+    window::{screen_height, screen_width},
+};
+
+use crate::{
+    constants::MOVEMENT_SPEED,
+    shared::{Collision, Coso, Organism},
+};
+
+pub enum TetroK {
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z,
+}
+
+pub struct Tetromino {
+    kind: TetroK,
+    pub props: Coso,
+}
+impl Tetromino {
+    pub(crate) fn from(spec: (TetroK, f32, f32)) -> Tetromino {
+        let (kind, x, y) = spec;
+        let color = match kind {
+            TetroK::I => SKYBLUE,
+            TetroK::J => DARKBLUE,
+            TetroK::L => ORANGE,
+            TetroK::O => YELLOW,
+            TetroK::S => DARKGREEN,
+            TetroK::T => PURPLE,
+            TetroK::Z => RED,
+        };
+        Tetromino {
+            kind,
+            props: Coso {
+                size: 52.0,
+                speed: MOVEMENT_SPEED,
+                x,
+                y,
+                collided: false,
+                color,
+            },
+        }
+    }
+}
+
+impl Organism for Tetromino {
+    fn update(&mut self) {
+        let delta_time = get_frame_time();
+        self.props.y += self.props.speed * delta_time;
+    }
+
+    fn draw(&mut self, block: &Vec2) {
+        draw_circle(
+            self.props.x * block.x,
+            self.props.y * block.y,
+            20.0,
+            self.props.color,
+        );
+    }
+
+    fn reset(&mut self) {
+        self.props.collided = false;
+        self.props.x = screen_width() / 2.0;
+        self.props.y = screen_height() / 2.0;
+    }
+}
+
+impl Collision for Tetromino {
+    fn collides_with(&self, other: &Rect) -> bool {
+        self.rect().overlaps(other)
+    }
+
+    //? el cuadro que mapea la colisión❗
+    /*
+     * Rect also starts from the upper left corner, so we must too here subtract half
+     * the stork from both X and Y.
+     *
+     * phau: falta un debug mode para ver el perímetro❗
+     */
+    //todo: draw helpers
+    fn rect(&self) -> Rect {
+        Rect {
+            x: self.props.x - self.props.size / 2.0,
+            y: self.props.y - self.props.size / 2.0,
+            w: self.props.size,
+            h: self.props.size,
+        }
+    }
+}
