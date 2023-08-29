a66b090c17771cf7be8578c5655a9823cf470693
phaunus <1057021+ph4un00b@users.noreply.github.com>
2023-08-28 18:53:58 -0600

    draw pieces

---
+++ src/tetrio_T.rs
@@ -0,0 +1,131 @@
+use macroquad::{prelude::Vec2, shapes::draw_rectangle};
+
+use crate::tetromino::Clock;
+
+pub struct TetrioT;
+impl TetrioT {
+    pub(crate) fn draw(t: &crate::tetromino::Tetromino, block: &Vec2) {
+        match t.rotation {
+            Clock::P12 => {
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    0. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    0. * block.x + (t.props.x * block.x),
+                    1. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    1. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    2. * block.x + (t.props.x * block.x),
+                    1. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+            }
+            Clock::P3 => {
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    0. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    1. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    2. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    2. * block.x + (t.props.x * block.x),
+                    1. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+            }
+            Clock::P6 => {
+                draw_rectangle(
+                    0. * block.x + (t.props.x * block.x),
+                    0. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    0. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    2. * block.x + (t.props.x * block.x),
+                    0. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    1. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+            }
+            Clock::P9 => {
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    0. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    1. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    1. * block.x + (t.props.x * block.x),
+                    2. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+                draw_rectangle(
+                    0. * block.x + (t.props.x * block.x),
+                    1. * block.y + (t.props.y * block.y),
+                    block.x,
+                    block.y,
+                    t.props.color,
+                );
+            }
+        }
+    }
+}
