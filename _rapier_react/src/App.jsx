b4ef99062b3a0f334d09c239c688671053b70014
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-08-31 19:55:44 -0600

    add test scene

--- _rapier_react/src/App.jsx
+++ _rapier_react/src/App.jsx
@@ -1,6 +1,11 @@
-import { Box, Sphere, Torus } from "@react-three/drei";
+import { Box, Sphere } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+import {
+  BallCollider,
+  CuboidCollider,
+  Physics,
+  RigidBody,
+} from "@react-three/rapier";
 import { Suspense } from "react";
 
 export default function App() {
@@ -8,13 +13,57 @@ export default function App() {
     <Canvas>
       <Suspense>
         <Physics debug={!false}>
-          <RigidBody colliders={"cuboid"} restitution={1.5}>
-            <Box />
-          </RigidBody>
-
-          <CuboidCollider position={[0, -2, 0]} args={[5, 0.5, 1]} />
+          <SensorScene />
+          {/* <BasicScene /> */}
         </Physics>
       </Suspense>
     </Canvas>
+  );
+}
+
+function SensorScene() {
+  return (
+    <>
+      <group position={[2, 5, 0]} rotation={[0, 0.3, 2]}>
+        <RigidBody>
+          <Box />
+          <CuboidCollider
+            args={[1.5, 0.5, 0.5]}
+            //? el sensor no genera collision!
+            sensor
+            onIntersectionEnter={() => console.log("le tocó ( •_•)>⌐■-■!")}
+          />
+        </RigidBody>
+      </group>
+
+      <BallCollider args={[0.5]} />
+      <CuboidCollider
+        position={[0, -2, 0]}
+        args={[5, 0.5, 2]}
+      />
+    </>
+  );
+}
+
+function BasicScene() {
+  return (
+    <>
+      <RigidBody colliders={"ball"} restitution={1.5} position={[2, 10, 0]}>
+        <Sphere />
+        <BallCollider args={[0.5]} position={[-2, 0, 0]} />
+        <BallCollider args={[0.5]} position={[2, 0, 0]} />
+      </RigidBody>
+
+      <RigidBody
+        position={[0, 6, 0]}
+        colliders={"cuboid"}
+        restitution={1.5}
+      >
+        <Box />
+      </RigidBody>
+
+      <BallCollider args={[0.5]} />
+      <CuboidCollider position={[0, -2, 0]} args={[10, 0.5, 1]} />
+    </>
   );
 }
